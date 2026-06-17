e349fe3de4c52bb2e7dcddb3423607244a60822a
marlboro-kid2 <chanpion.no1@icloud.com>
2026-06-17 16:52:10 +0900

    q

---
+++ Assets/Ritsuki/screen_sc.cs
@@ -0,0 +1,13 @@
+﻿using UdonSharp;
+using UnityEngine;
+
+public class screen_sc : UdonSharpBehaviour
+{
+
+    public GameObject screen;
+
+    public override void Interact()
+    {
+        screen.SetActive(true);
+    }
+}
